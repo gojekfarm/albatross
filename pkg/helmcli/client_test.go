ab9bfe8ea6d8b84e79cdd8b2eaef9273fad6c93d
Karthik Yelchuru <yelkarthik@gmail.com>
2020-10-26 11:25:00 +0530

    Add tests to validation namespace, dryrun flags as well for installer and upgrader

--- pkg/helmcli/client_test.go
+++ pkg/helmcli/client_test.go
@@ -11,26 +11,56 @@ import (
 
 type TestSuite struct {
 	suite.Suite
-	c       Client
-	version string
+	c Client
 }
 
 func (s *TestSuite) SetupTest() {
 	s.c = New()
-	s.version = "0.1.0"
 }
 
 func (s *TestSuite) TestNewUpgraderSetsChartOptionsUsingFlagValues() {
 	t := s.T()
+	version := "0.1.0"
+	dryRun := false
 	install := false
-	flg := flags.UpgradeFlags{
-		Version: s.version,
-		Install: install,
+	globalFlags := flags.GlobalFlags{
+		Namespace: "namespace",
 	}
-	u, _ := s.c.NewUpgrader(flg)
-	newUpgrader, _ := u.(*upgrader)
-	assert.Equal(t, s.version, newUpgrader.action.Version)
+	flg := flags.UpgradeFlags{
+		Version:     version,
+		Install:     install,
+		DryRun:      dryRun,
+		GlobalFlags: globalFlags,
+	}
+	u, err := s.c.NewUpgrader(flg)
+	newUpgrader, ok := u.(*upgrader)
+	assert.NoError(t, err)
+	assert.True(t, ok)
+	assert.Equal(t, version, newUpgrader.action.Version)
 	assert.Equal(t, install, newUpgrader.action.Install)
+	assert.Equal(t, dryRun, newUpgrader.action.DryRun)
+	assert.Equal(t, globalFlags.Namespace, newUpgrader.action.Namespace)
+}
+
+func (s *TestSuite) TestNewInstallerSetsChartOptionsUsingFlagValues() {
+	t := s.T()
+	version := "0.1.0"
+	dryRun := false
+	globalFlags := flags.GlobalFlags{
+		Namespace: "namespace",
+	}
+	flg := flags.InstallFlags{
+		Version:     version,
+		DryRun:      dryRun,
+		GlobalFlags: globalFlags,
+	}
+	i, err := s.c.NewInstaller(flg)
+	newInstaller, ok := i.(*installer)
+	assert.NoError(t, err)
+	assert.True(t, ok)
+	assert.Equal(t, version, newInstaller.action.Version)
+	assert.Equal(t, dryRun, newInstaller.action.DryRun)
+	assert.Equal(t, globalFlags.Namespace, newInstaller.action.Namespace)
 }
 
 func TestHandler(t *testing.T) {
