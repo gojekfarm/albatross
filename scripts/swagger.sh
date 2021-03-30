00ddea0d280d44b1daf4f8f49e2a49f50e144380
punit-kulal <punit.kulal@gojek.com>
2021-03-30 20:16:22 +0530

    updated script for doc-ci

--- scripts/swagger.sh
+++ scripts/swagger.sh
@@ -20,12 +20,13 @@ check_for_change() {
 }
 
 check_doc_ci() {
+    mkdir bin
     download_url=$(curl -s https://api.github.com/repos/go-swagger/go-swagger/releases/latest | \
     jq -r '.assets[] | select(.name | contains("'"$(uname | tr '[:upper:]' '[:lower:]')"'_amd64")) | .browser_download_url')
-    curl -o /usr/local/bin/swagger -L'#' "$download_url"
-    chmod +x /usr/local/bin/swagger
-    swagger generate spec -m -o docs/swagger.json --exclude-deps
-    swagger validate docs/swagger.json
+    curl -o ./bin/swagger -L'#' "$download_url"
+    chmod +x ./bin/swagger
+    ./bin/swagger generate spec -m -o docs/swagger.json --exclude-deps
+    ./bin/swagger validate docs/swagger.json
 }
 
 $*
