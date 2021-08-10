d2ab1357cc86e159a16eb48942a81be62e5afcba
Punit Kulal <punitkulal1996@gmail.com>
2021-08-10 13:20:20 +0530

    Finalise Release API spec  (#12)
    * add endpoint to check status of an individual release
    
    * updated api spec for install and update actions
    
    * set http status code as not found(404) for releases that do not exist instead of 204
    
    Co-authored-by: Kaustubh Kurve <kaustubh6024@gmail.com>

--- cmd/albatross/albatross.go
+++ cmd/albatross/albatross.go
@@ -11,6 +11,7 @@ import (
 	"github.com/gojekfarm/albatross/api"
 	"github.com/gojekfarm/albatross/api/install"
 	"github.com/gojekfarm/albatross/api/list"
+	"github.com/gojekfarm/albatross/api/status"
 	"github.com/gojekfarm/albatross/api/uninstall"
 	"github.com/gojekfarm/albatross/api/upgrade"
 	"github.com/gojekfarm/albatross/pkg/helmcli"
@@ -40,13 +41,15 @@ func startServer() {
 	upgradeHandler := upgrade.Handler(upgrade.NewService(cli))
 	listHandler := list.Handler(list.NewService(cli))
 	uninstallHandler := uninstall.Handler(uninstall.NewService(cli))
+	statusHandler := status.Handler(status.NewService(cli))
 
 	router.Handle("/ping", ContentTypeMiddle(api.Ping())).Methods(http.MethodGet)
 	router.Handle("/clusters/{cluster}/namespaces/{namespace}/releases/{release_name}", ContentTypeMiddle(uninstallHandler)).Methods(http.MethodDelete)
-	router.Handle("/clusters/{cluster}/namespaces/{namespace}/releases/{release_name}", ContentTypeMiddle(installHandler)).Methods(http.MethodPut)
-	router.Handle("/clusters/{cluster}/namespaces/{namespace}/releases/{release_name}", ContentTypeMiddle(upgradeHandler)).Methods(http.MethodPost)
+	router.Handle("/clusters/{cluster}/namespaces/{namespace}/releases", ContentTypeMiddle(installHandler)).Methods(http.MethodPost)
+	router.Handle("/clusters/{cluster}/namespaces/{namespace}/releases/{release_name}", ContentTypeMiddle(upgradeHandler)).Methods(http.MethodPut)
 	router.Handle("/clusters/{cluster}/releases", ContentTypeMiddle(listHandler)).Methods(http.MethodGet)
 	router.Handle("/clusters/{cluster}/namespaces/{namespace}/releases", ContentTypeMiddle(listHandler)).Methods(http.MethodGet)
+	router.Handle("/clusters/{cluster}/namespaces/{namespace}/releases/{release_name}", ContentTypeMiddle(statusHandler)).Methods(http.MethodGet)
 
 	serveDocumentation(router)
 	err := http.ListenAndServe(fmt.Sprintf(":%d", 8080), router)
