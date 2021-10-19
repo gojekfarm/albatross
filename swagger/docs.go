fb384c1881a93b07b2f127547b7e46643dfa3d62
punit-kulal <punit.kulal@gojek.com>
2021-10-19 16:28:43 +0530

    Bump version to v1.1.0

--- swagger/docs.go
+++ swagger/docs.go
@@ -3,7 +3,7 @@
 // Albatross is a helm cli wrapper and enables using helm via http calls
 //
 //     Schemes: http
-//     Version: v1.0.3
+//     Version: v1.1.0
 //
 //     Consumes:
 //     - application/json
